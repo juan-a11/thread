ab8fd87b832099a47e2267f15e5db82a2e18f234
ops <oscar@ebron.tech>
2019-11-27 20:11:10 -0700

    FIrst commit

--- main.cpp
+++ main.cpp
@@ -1,27 +1,31 @@
 #include <iostream>
 #include <pthread.h>
 #include <math.h>
+#include <ctime>
 using namespace std;
 
 #define NUM_THREADS 500
 
 void * PrintHelloWolrd(void *threadis){
+    clock_t timer = clock();
     long tid;
     tid = (long) threadis;
     float aux=0;
     for(int i =0; i<8000000;i++){
         aux+= sin(i/12345);
     }
-    cout << "Hello World Thread ID " << tid <<endl;
+    timer = clock () - timer;
+    cout << "Hello World Thread ID " << tid << " tardo " << timer/CLOCKS_PER_SEC << "seg" << endl;
     pthread_exit(NULL);
 }
 
 int main() {
     pthread_t threads[NUM_THREADS];
     int rc, i;
-
+    clock_t timer_total = clock();
     for (int i =0; i< NUM_THREADS;i++){
         cout << "main(): creating thread, " << i <<endl;
+
         rc = pthread_create(&threads[i],NULL,PrintHelloWolrd,(void*)i);
 
         if(rc){
@@ -29,6 +33,11 @@ int main() {
             exit(-1);
         }
     }
+    for (int i =0; i< NUM_THREADS;i++){
+        pthread_join(threads[i],NULL);
+    }
+    timer_total = clock () - timer_total;
+    cout << "Se tardo " << timer_total / CLOCKS_PER_SEC << " segundos en completarse";
     pthread_exit(NULL);
     //cout << "Hello World\n";
     
